ea6c1893a64b919f22fa96de699a7cbfd91bd0a3
Nikhil Arora <narora0811@gmail.com>
2019-10-23 21:30:29 -0700

    Finished setting up Node.js Express server

--- src/app.js
+++ src/app.js
@@ -1,13 +1,35 @@
+import ThreeContainer from "./components/ThreeContainer.js"; 
 import React, { Component} from "react";
 import {hot} from "react-hot-loader";
 
-import ThreeContainer from "./components/ThreeContainer.js"; 
-
 class App extends Component{
+
+state = {
+        data: null
+};
+
+componentDidMount() {
+    // Call our fetch function below once the component mounts
+    this.callBackendAPI()
+      .then(res => this.setState({ data: res.express }))
+      .catch(err => console.log(err));
+  }
+
+ // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
+  callBackendAPI = async () => {
+    const response = await fetch('/express_backend');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message)
+    }
+    return body;
+  };
+
   render(){
     return(
       <div className="App">
-        <ThreeContainer /> 
+        <ThreeContainer />
       </div>
     );
   }
